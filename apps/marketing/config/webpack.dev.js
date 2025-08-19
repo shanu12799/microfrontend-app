2d94b39956baea89ac3c73e449915676788a5f3d
Sachin Gupta <ggsachin12799@gmail.com>
2025-08-20 04:42:00 +0530

    prod config | container | marketing

--- apps/marketing/config/webpack.dev.js
+++ apps/marketing/config/webpack.dev.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
+const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   devServer: {
@@ -18,7 +18,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstarp",
       },
-      shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
